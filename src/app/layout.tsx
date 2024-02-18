809cfb6af10cdfcb5aa0b17d19cf80ee8c9052f8
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-02-18 13:34:57 +0100

    update readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Simple Reader View",
-  description: "Enter a URL to get a simplified view of the page's content",
+  description: "Enter a URL to get a simplified view of a page's content",
 };
 
 export default function RootLayout({
